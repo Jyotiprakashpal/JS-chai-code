8d3dfa9cf3bed722f597394992bab824235e3061
Jyotiprakash pal <154493906+Jyotiprakashpal@users.noreply.github.com>
2024-12-29 19:06:10 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14454
+let accountEmail = "jyoti2021@gift.edu.in"
+var accountPassword = "12345"
+accountcity = "Bhubaneswar"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "hs@hc@gmail.com"
+accountPassword = "1213141"
+accountcity ="balasore"
+
+console.log(accountId);
+/*
+    prefered not to use "var"
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
