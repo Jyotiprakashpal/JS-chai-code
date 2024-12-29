a69d5f609d0b59c7e9d5bc6cac6b96bba68d1ba3
Jyotiprakash pal <154493906+Jyotiprakashpal@users.noreply.github.com>
2024-12-29 19:07:22 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer code
+
+//alert(3+3)
+//we are using node js not browser
+console.log(3+3)
+console.log("jyotiprakash")
+
+let name = "jyotiprakash pal"
+let age = 18
+let LoggedIn = false
+
+//number = 2 to power 23
+//bigint
+//string =>""
+//bolean =>true/false
+//null=>standalone value
+//undefined
+//object
+
+console.log(typeof "Jyotiprakash");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
