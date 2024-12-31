e2edcf90a63a0e8bef6787646899493ad9cccff0
Jyotiprakash pal <154493906+Jyotiprakashpal@users.noreply.github.com>
2024-12-31 09:46:36 +0000

    operation

---
+++ 01_Basics/03_operation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = " jyoti"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1 + 2)
+
+console.log(+true);
+console.log(+ "");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
