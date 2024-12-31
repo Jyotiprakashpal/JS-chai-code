f6f93ae16f1c656447c54c5266d951abecc8485f
Jyotiprakash pal <154493906+Jyotiprakashpal@users.noreply.github.com>
2024-12-31 01:38:31 +0000

    converation

---
+++ 01_Basics/03_conversation.js
@@ -0,0 +1,32 @@
+let score1 = "jyotipraksh"
+
+//console.log(typeof score1)
+//console.log(typeof (score1));
+
+// let score = 33
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let ValueNumber = Number(score1)
+//console.log(typeof ValueNumber)
+//console.log(ValueNumber)
+
+// "33" => 33
+//"33abc" => NaN(not a number)
+//ture => 1
+//false => 0
+
+let isLoggedIn = "jyoti"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" =>false
+//"jyoti" =>true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
